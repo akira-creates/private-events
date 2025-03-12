194d213c2662d009d8f9af8b720a54e2c22ec28e
Luke Webb <lukewebb2000@gmail.com>
2025-03-12 21:58:16 +1300

    Create migration for creators to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_05_233602) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_234417) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_05_233602) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
