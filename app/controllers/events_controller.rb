23b5c747a0335da05b9bb0936efa94f07eb5bf79
Luke Webb <lukewebb2000@gmail.com>
2025-03-31 06:18:01 +1300

    Implement event management features: add CRUD actions in EventsController, create views for event creation and display, and update routes.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,31 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+    @creator = User.find(@event.creator_id)
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @creator = @event.build_creator(id: current_user.id)
+    if @event.save
+      redirect_to @event, notice: "Event was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :date)
+  end
 end
