f241635b72feecf3739702637bb9e8bfac57b061
Luke Webb <lukewebb2000@gmail.com>
2025-04-22 16:30:31 +1200

    Add attendance model and associations for event participation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :users, only: [ :show ]
   resources :events
   get "events/index"
+
+  resouces :attendances, only: [ :create, :destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
