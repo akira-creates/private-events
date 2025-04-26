cc09b1840c08b5af7235e49cecc02a5d14855aed
Luke Webb <lukewebb2000@gmail.com>
2025-04-26 19:41:47 +1200

    Refactor event display: create a partial for event sections and update index view to use it for better organization and readability.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :attendances, source: :attendee
+
+  def self.past
+    where("date < ?", Time.current)
+  end
+
+  def self.future
+    where("date >= ?", Time.current)
+  end
 end
